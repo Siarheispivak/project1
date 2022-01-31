8cfd18260496c4c1ab7fffd32c471bb44e29c707
Siarheispivak <siarheispivak2@gmail.com>
2022-01-31 19:03:53 +0300

    first commette

---
+++ vs 1/js/main.js
@@ -0,0 +1,46 @@
+// setTimeout(wakeUpUser, 5000);
+// function wakeUpUser() {
+//     alert("are you going stay on this page foever?");
+// }
+
+
+// let scoops = 2;
+// if (scoops < 3) {
+//     alert("Ice creame is running low!");
+// } else {
+//     alert("nice");
+// }
+
+
+// let scoops = 3;
+// if (scoops >= 5) {
+//     alert("eat faster!");
+// } else if (scoops >= 3) {
+//     alert("ice cream is running low");
+// }
+
+// let message = "Howdy" + " " + "partner";
+// console.log(message);
+
+// let word = "bottles";
+// let count = 9;
+// while (count > 0) {
+//     console.log(count + " " + word + " of beer on the wall");
+//     console.log(count + " " + word + " of beer,");
+//     console.log("take one down, pass it around,");
+//     count = count - 1;
+//     if (count > 0) {
+//         console.log(count + " " + word + " of beer on the wall");
+//     } else {
+//         console.log("No more " + word + " of beer on the wall.");
+//     }
+
+// }
+
+// let location1 = 3;
+// let location2 = 4;
+// let location3 = 5;
+// let guess = 0;
+// let hits = 0;
+// let guesses = 0;
+// let isSunk = false;
